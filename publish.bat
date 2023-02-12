054016b64db8381a01fad652e6a16534091f4df3
Gerren <voldrichv@gmail.com>
2023-02-12 07:40:12 +0100

    manifests

---
+++ publish.bat
@@ -0,0 +1,8 @@
+﻿d:\Playnite\Toolbox.exe verify Installer "e:\Zaloha\Projects\Playnite\MindGame\MindGame\installer.yaml"
+d:\Playnite\Toolbox.exe verify Addon "e:\Zaloha\Projects\Playnite\MindGame\MindGame\Gerren_MindGame.yaml"
+pause
+copy /Y "e:\Zaloha\Projects\Playnite\MindGame\MindGame\extension.yaml" "E:\Zaloha\Projects\Playnite\MindGame\MindGame\bin\Debug\extension.yaml"
+d:\Playnite\Toolbox.exe pack "E:\Zaloha\Projects\Playnite\MindGame\MindGame\bin\Debug" "E:\Zaloha\Projects\Playnite"
+
+explorer "E:\Zaloha\Projects\Playnite"
+explorer "https://github.com/Gerren/Playnite-MindGame/upload/master"
--- validate.bat
+++
@@ -1,3 +0,0 @@
-﻿d:\Playnite\Toolbox.exe verify Installer "e:\Zaloha\Projects\Playnite\MindGame\MindGame\installer.yaml"
-d:\Playnite\Toolbox.exe verify Addon "e:\Zaloha\Projects\Playnite\MindGame\MindGame\Gerren_MindGame.yaml"
-pause
