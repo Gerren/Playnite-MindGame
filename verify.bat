1a4089770cee95302400cda4cb8639af4b0febd8
Gerren <voldrichv@gmail.com>
2023-02-12 08:28:02 +0100

    checklist, verify encoding "fix"

--- verify.bat
+++ verify.bat
@@ -1,3 +1,4 @@
-﻿d:\Playnite\Toolbox.exe verify Installer "e:\Zaloha\Projects\Playnite\MindGame\MindGame\installer.yaml"
+﻿@echo
+d:\Playnite\Toolbox.exe verify Installer "e:\Zaloha\Projects\Playnite\MindGame\MindGame\installer.yaml"
 d:\Playnite\Toolbox.exe verify Addon "e:\Zaloha\Projects\Playnite\MindGame\MindGame\Gerren_MindGame.yaml"
 pause
